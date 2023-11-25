97ea97f16089afd8d4537db44cf9f5cea1abe02b
SebaTapiaG <sebastian.tapia.ga@usach.cl>
2023-11-25 13:14:54 -0300

    Primer commit

---
+++ Option.java
@@ -0,0 +1,59 @@
+package org.example.lab3_21022807_Tapia;
+
+import java.util.List;
+
+public class Option {
+    private int code;
+    private String message;
+    private int ChatbotCodeLink;
+    private int InitialFlowCodeLink;
+    private List<String> Keyword;
+
+    public Option(int code, String message, int ChatbotCodeLink, int InitialFlowCodeLink, List<String> Keyword) {
+        this.code = code;
+        this.message = message;
+        this.ChatbotCodeLink = ChatbotCodeLink;
+        this.InitialFlowCodeLink = InitialFlowCodeLink;
+        this.Keyword = Keyword;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public int getChatbotCodeLink() {
+        return ChatbotCodeLink;
+    }
+
+    public int getInitialFlowCodeLink() {
+        return InitialFlowCodeLink;
+    }
+
+    public List<String> getKeyword() {
+        return Keyword;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;  // Verifica si es la misma instancia
+        if (obj == null) return false;  // Verifica si las clases son diferentes
+
+        Option auxOption = (Option) obj;  // Hace un cast a Option
+
+        // Compara los códigos utilizando equals()
+        return getCode() == auxOption.getCode();
+    }
+
+    @Override
+    public String toString() {
+        return this.code + " " + this.message + " " + this.Keyword;
+    }
+
+
+}
+
+
